21d0ee8125c394eca87d79abb94163c5fde82404
roopatm <roopatm6@gmail.com>
2022-06-01 02:05:38 -0700

    code pushed

---
+++ GitPractice/src/test/java/com/gitpractice/git/TestAnnotation.java
@@ -0,0 +1,14 @@
+package com.gitpractice.git;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestAnnotation 
+{
+@Test
+ public void testcase()
+ {
+	Reporter.log("my 1st test case",true);
+ }
+	
+}
