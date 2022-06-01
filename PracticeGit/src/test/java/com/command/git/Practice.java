b6228ef56acb6fbf58950eb36a2a0f6a1f1f016f
roopatm <roopatm6@gmail.com>
2022-06-01 02:41:23 -0700

    code pushed

---
+++ PracticeGit/src/test/java/com/command/git/Practice.java
@@ -0,0 +1,15 @@
+package com.command.git;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Practice 
+{
+
+	@Test                                       
+	 public void testcase()                     
+	 {                                          
+		Reporter.log("my 1st test case",true);  
+	 }                                          	
+	
+}
